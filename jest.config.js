751f66a5bc607de5bcd73f34ecb8e752f4aa3a55
Abdelrahman Mostafa <abdelrahmandeveloper123@gmail.com>
2025-06-13 13:41:46 +0200

    Updates ts-jest configuration for React JSX support

--- jest.config.js
+++ jest.config.js
@@ -4,12 +4,11 @@ module.exports = {
   testEnvironment: 'jsdom',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.json',
-    },
+    '^.+\\.(ts|tsx)$': ['ts-jest', {
+      tsconfig: {
+        jsx: 'react-jsx'
+      }
+    }]
   },
   moduleDirectories: ['node_modules', 'src'],
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
